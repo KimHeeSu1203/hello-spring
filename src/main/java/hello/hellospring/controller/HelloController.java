c1bfff7132aedd52623a07d91ef08d7cd5330723
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-01-16 17:27:48 +0900

    220116 / Spring / API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,6 +18,36 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){ // ?name= ~~~ 의 값이 name에 들어가고
         model.addAttribute("name",name); //name에 들어간 값이 name이름으로 넘어감
         return "hello-template";
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody // http body에 직접 접근하겠다는 것
+    public String helloSpring(@RequestParam("name") String name){
+        return "hello " + name;
+
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+
+
+    static class Hello{
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private String name;
 
     }
 }
