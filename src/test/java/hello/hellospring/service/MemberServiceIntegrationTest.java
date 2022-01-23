6e870a710defe8c65d8e5494e4ab0cc658611577
KimHeeSu1203 <ozo789@sookmyung.ac.kr>
2022-01-23 16:20:11 +0900

    220123 / Spring / JPA

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -24,7 +25,7 @@ class MemberServiceIntegrationTest {
     void 회원가입() {
         //given -> 이걸 가지고 하는거구나
         Member member = new Member();
-        member.setName("spring");
+        member.setName("spring100");
 
         //when -> 이걸 검증하는구나
         Long saveId = memberService.join(member);
